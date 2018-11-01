844e81553cb38ecad1974b5d7751598bbd2303a7
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-11-01 22:34:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,21 @@
-
-
-def line(katz_deli)
-  if katz_deli.length == 0 
-   puts "The line is currently empty."
-   elsif katz_deli.length >= 1
-   puts "The line is currently: "
-   
+def line(katz)
+  katz_deli = []
+  if katz.length == 0
+  puts "The line is currently empty."
+  elsif katz.length >= 1
+  katz.each_with_index do |i, index|
+    index_plus_one = index + 1
+    katz_deli << "#{index_plus_one}. #{i}"
+end
+puts "The line is currently: " + katz_deli.join(" ")
 end
 end
+
+def take_a_number(katz_deli, name)
+ if katz_deli.length == 0 
+   katz_deli.push(name)
+   puts "Welcome, #{name}"
+end    
+end 
+
+
