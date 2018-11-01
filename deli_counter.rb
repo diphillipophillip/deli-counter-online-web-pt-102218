3c515e793c021788347ce6d94561112ab058df7e
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-11-01 22:40:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(katz_deli, name)
    puts "Welcome, #{name}. You are number 1 in line."
    elsif katz_deli.length == 3 
    katz_deli.push(name)
-   puts "Welcome, #{name}, You are number 4 in line."
+   puts "Welcome, #{name}. You are number 4 in line."
    
 end    
 end 
